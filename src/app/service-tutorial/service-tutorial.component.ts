6b27c1b93bb1aedfb833e1194b626f1ca807a84b
Valentina Kuxhausen <valentina.kuxhausen@fino.digital>
2022-07-12 15:41:00 +0200

    comment

--- src/app/service-tutorial/service-tutorial.component.ts
+++ src/app/service-tutorial/service-tutorial.component.ts
@@ -22,7 +22,7 @@ export class ServiceTutorialComponent implements OnInit {
   constructor(private dservice: DataService) { }
 
   ngOnInit(): void {
-    test test test
+    // test test test
   }
 
 }
